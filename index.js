1120dd5ea6f831c8f82c7f8570c8d65de8148cdd
Pla Athitiya <Athitiya.pla@gmail.com>
2023-06-03 21:57:00 +0700

    remove comment in js

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ function call() {
     window.open('tel:+66890802004', '_blank')
 }
 
-// window.addEventListener('resize', function () {
-//     const row1 = document.getElementById('pla1')
-//     const row2 = document.getElementById('pla2')
-//     if (this.screen.width < 600) {
-//         row1.classList.remove('row')
-//         row2.classList.remove('row')
-//         row1.classList.add('row-responsive')
-//         row2.classList.add('row-responsive')
-//     }
-//     else {
-//         row1.classList.remove('row-responsive')
-//         row2.classList.remove('row-responsive')
-//         row1.classList.add('row')
-//         row2.classList.add('row')
-//     }
-// })
+window.addEventListener('resize', function () {
+    const row1 = document.getElementById('pla1')
+    const row2 = document.getElementById('pla2')
+    if (this.screen.width < 600) {
+        row1.classList.remove('row')
+        row2.classList.remove('row')
+        row1.classList.add('row-responsive')
+        row2.classList.add('row-responsive')
+    }
+    else {
+        row1.classList.remove('row-responsive')
+        row2.classList.remove('row-responsive')
+        row1.classList.add('row')
+        row2.classList.add('row')
+    }
+})
 
 
 
